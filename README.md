aac20f4da130881b5f4fe752de51d92eadf57c11
eeerpjw <976118690@qq.com>
2021-01-13 21:28:05 +0800

    加入开机启动

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
    - 收件人字典列表
    - 发件邮箱和授权码
    - 如果使用的不是163邮箱，需要更改host和port
+   - log、存放最新ip文件的地址、doit函数中python的环境地址都要改为绝对地址
    
 > 如何申请163邮箱的授权码
 > [教程](https://help.mail.163.com/faqDetail.do?code=d7a5dc8471cd0c0e8b4b8f4f8e49998b374173cfe9171305fa1ce630d7f67ac2cda80145a1742516)
@@ -21,3 +22,17 @@ python main.py  # 默认每0.5小时查看一次ip变更
 python main.py --freq 1 # 每一小时查看一次IP是否变更
 ```
 > 建议开[screen](https://www.cnblogs.com/mchina/archive/2013/01/30/2880680.html)后运行
+
+> 也可以使用nohub COMMAND & 运行，具体参考[run_silently.py](./run_silently.py)
+
+3. 设置开机启动（需要sudo权限）
+```bash
+sudo vim /etc/rc.local
+```
+在exit 0 前面加入：
+```bash
+sudo -H -u pjw nohup $PATH_PYTHON -u $PATH_TO_MAIN_FILE --freq=0.5 > $HOME/rebootip.log 2>&1 &
+```
+参考：
+- [开机启动python脚本](https://blog.csdn.net/adcadc123456789/article/details/107201140)
+- [Linux 开机启动配置 rc.local 文件运行 python 脚本的 ImportError:No module named xxx](http://deanlib.com/index.php/archives/26/)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 import time
 from threading import Timer
 from datetime import datetime
-
+from restart_notification import send_noti
 def get_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--freq", type=float, default=0.5, help="frequency of checking IP")
@@ -11,7 +11,9 @@ def get_opt():
     return opt
 
 def doit():
-    os.system("python check_send.py")
+    # 开机启动的话这里需要将python的地址改为绝对路径，
+    # 指定python环境位置，否则会默认系统自带的python，导致出错
+    os.system("python check_send.py") 
     intertime = opt.freq*60*60
     t = Timer(intertime, doit)#每86400秒（1天），发送1次，实际要86420-6240，因为每次发送大概持续2小时，不用linux的定时任务是因为每次登陆都需要扫描二维码登陆，很麻烦的一件事，就让他一直挂着吧
     t.start()
@@ -19,4 +21,11 @@ def doit():
 if __name__ == '__main__':
     opt = get_opt()
     print('check IP every %s hour(s)'%opt.freq)
+    s = datetime.now()
+    # 不能开机后立即执行程序，会导致找找不到requests等packages问题
+    print('NOW:%s，Send will start in %s minutes'%(s,6))
+    time.sleep(60*6)
+    # 发送机器重启的提醒
+    send_noti("9xxxxxxx0@qq.com")# 发送启动的消息
+    # 定期检查ip变化并发送邮件
     doit()
